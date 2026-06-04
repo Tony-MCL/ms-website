61c7423d0d09f0a95fcc23d4da310af003657a0b
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-06-04 08:09:29 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,18 @@ import "./styles/styles.css";
 import { I18nProvider } from "./i18n/I18nProvider";
 import ErrorBoundary from "./ErrorBoundary";
 
+function redirectOldHashRoutes() {
+  const hash = window.location.hash;
+
+  if (!hash.startsWith("#/")) return;
+
+  const newPath = hash.substring(1);
+
+  window.location.replace(newPath);
+}
+
+redirectOldHashRoutes();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary>
